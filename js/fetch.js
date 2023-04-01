47d13c011a379e879e31fd6f524d487da01f3c42
Dimas Ardianto <dimasardnt6@gmail.com>
2023-04-01 13:37:07 +0700

    push

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,25 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { setInner } from "https://bukulapak.github.io/element/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./tabel.js";
 let urlAPI = "https://dimasardnt6-ulbi.herokuapp.com/all-kemahasiswaan";
-get(urlAPI,isiTablePresensi);
-function isiTablePresensi(results){
-    console.log(results);
+get(urlAPI, isiTablePresensi);
+function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
-function isiRow(value){
-    console.log(value)
+function isiRow(value) {
+    let content = 
+    isiTabel.replace("#NPM#", value.identitas.npm)
+            .replace("#NAMA#", value.identitas.nama)
+            .replace("#NOHP#", value.identitas.no_hp)
+            .replace("#NAMAPRODI#", value.identitas.prodi.nama)
+            .replace("#JURUSAN#", value.identitas.jurusan)
+            .replace("#PEMBAYARAN#", value.status_keuangan.total_pembayaran)
+            .replace("#MATKUL#", value.nilai_mhs.matakuliah.nama_matkul)
+            .replace("#DOSEN#", value.nilai_mhs.matakuliah.dosen)
+            .replace("#ANGKA#", value.nilai_mhs.nilai_angka)
+            .replace("#HURUF#", value.nilai_mhs.nilai_huruf)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
 }
