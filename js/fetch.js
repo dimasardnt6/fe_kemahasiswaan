4f2f04f1216b0ad4d542773779f255e1786831f5
Dimas Ardianto <dimasardnt6@gmail.com>
2023-04-01 14:36:22 +0700

    push

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,4 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { isiTableKemahasiswaan } from "./controller/get.js";
+import { isiTable } from "./controller/get.js";
 import { urlAPI } from "./config/url.js";
-get(urlAPI, isiTableKemahasiswaan);
+get(urlAPI, isiTable);
