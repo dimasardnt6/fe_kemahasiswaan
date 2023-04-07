d68374ab1d2800c13fc395dce088c02b5172896f
Dimas Ardianto <dimasardnt6@gmail.com>
2023-04-07 15:20:57 +0700

    push

--- js/config/url.js
+++ js/config/url.js
@@ -1,1 +1,6 @@
 export let urlAPI = "https://dimasardnt6-ulbi.herokuapp.com/all-kemahasiswaan";
+
+export let urlAPIKemahasiswaan = "https://dimasardnt6-ulbi.herokuapp.com/all-kemahasiswaan";
+export let urlAPIMahasiswa = "https://dimasardnt6-ulbi.herokuapp.com/all-mahasiswa";
+export let urlAPIKeuangan = "https://dimasardnt6-ulbi.herokuapp.com/all-keuangan";
+export let urlAPINilai = "https://dimasardnt6-ulbi.herokuapp.com/all-nilai";
