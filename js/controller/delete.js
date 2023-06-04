1bae5f709f2d3fc65e97c9033fc8b59c45ea6068
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-04 23:44:07 +0700

    push

--- js/controller/delete.js
+++ js/controller/delete.js
@@ -1,6 +1,6 @@
 function deleteData(IDHAPUS) {
     var kemahasiswaanId = IDHAPUS;
-    var target_url = "https://dimasardnt6-ulbi.herokuapp.com/delete-kemahasiswaan" + kemahasiswaanId;
+    var target_url = "https://dimasardnt6-ulbi.herokuapp.com/delete-kemahasiswaan/" + kemahasiswaanId;
 
     var requestOptions = {
         method: 'DELETE',
