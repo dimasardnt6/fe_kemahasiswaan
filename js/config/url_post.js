c2739c44f296b7e804bc9cdfb21e1fd23dbab261
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-04 15:17:53 +0700

    push

--- js/config/url_post.js
+++ js/config/url_post.js
@@ -2,6 +2,6 @@ export let urlPOST = "https://dimasardnt6-ulbi.herokuapp.com/ins-kemahasiswaan"
 
 export function AmbilResponse(result) {
     console.log(result); //menampilkan response API pada console
-    console.log(result.message); //menampilkan response API pada alert
+    window.alert(result.message); //menampilkan response API pada alert
     window.location.reload(); //reload halaman setelah klik ok pada alert
 }
